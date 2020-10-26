de59a75b55bdbc7fb8b9d003753e086fc1ae04ad
tekky02 <1657961253@qq.com>
2020-10-26 12:21:10 +0800

    modified

---
+++ leetcode/Golang/641_design_circula_deque.go
@@ -0,0 +1,106 @@
+/**
+ * @ Author: tekky
+ * @ Create Time: 2020-10-26 12:20:19
+ * @ Modified by: tekky
+ * @ Modified time: 2020-10-26 12:20:39
+ */
+
+type MyCircularDeque struct {
+	size int
+	item []int
+	head int
+	tail int
+}
+
+func mod(d, m int) int {
+	var res int = d % m
+	if (res < 0 && m > 0) || (res > 0 && m < 0) {
+		return res + m
+	}
+	return res
+}
+
+/** Initialize your data structure here. Set the size of the deque to be k. */
+func Constructor(k int) MyCircularDeque {
+	return MyCircularDeque{
+		size: k + 1,
+		item: make([]int, k+1),
+	}
+}
+
+/** Adds an item at the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertFront(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	this.head = mod(this.head-1, this.size)
+	this.item[this.head] = value
+	return true
+}
+
+/** Adds an item at the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertLast(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	this.item[this.tail] = value
+	this.tail = mod(this.tail+1, this.size)
+	return true
+}
+
+/** Deletes an item from the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteFront() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	this.head = mod(this.head+1, this.size)
+	return true
+}
+
+/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteLast() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	this.tail = mod(this.tail-1, this.size)
+	return true
+}
+
+/** Get the front item from the deque. */
+func (this *MyCircularDeque) GetFront() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.item[this.head]
+}
+
+/** Get the last item from the deque. */
+func (this *MyCircularDeque) GetRear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.item[mod(this.tail-1, this.size)]
+}
+
+/** Checks whether the circular deque is empty or not. */
+func (this *MyCircularDeque) IsEmpty() bool {
+	return this.head == this.tail
+}
+
+/** Checks whether the circular deque is full or not. */
+func (this *MyCircularDeque) IsFull() bool {
+	return mod(this.tail+1, this.size) == this.head
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.InsertFront(value);
+ * param_2 := obj.InsertLast(value);
+ * param_3 := obj.DeleteFront();
+ * param_4 := obj.DeleteLast();
+ * param_5 := obj.GetFront();
+ * param_6 := obj.GetRear();
+ * param_7 := obj.IsEmpty();
+ * param_8 := obj.IsFull();
+ */
