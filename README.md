019c87f1d158fc9bab61d9f31ef45496b214fac2
tekky02 <1657961253@qq.com>
2020-10-26 10:41:03 +0800

    modified

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Algorithm
 开启刷题之旅了~
 记录一下自己做过的题目，方便复习之需
+## [我在LeetCode]("https://leetcode.com/ktyy/")
---
+++ leetcode/C++/169_majority_element.cc
@@ -0,0 +1,9 @@
+/**
+ * @ Author: tekky
+ * @ Create Time: 2020-10-26 10:37:53
+ * @ Modified by: tekky
+ * @ Modified time: 2020-10-26 10:38:09
+ * @ Description:find the majority element in an array
+ * @ Note: it is guaranteed an element appears more than n/2 times.
+ */
+
