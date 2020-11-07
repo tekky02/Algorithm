35dcd270023581cf281630d665bd0840c9fe086a
Teddyky <1657961253@qq.com>
2020-11-07 12:06:06 +0800

    modified

---
+++ other/POJ/1608_parencodings.cc
@@ -0,0 +1,19 @@
+// 1608_parencodings.cc
+// created by tekky on 2020.11.5.
+
+#include <cstdio>
+#include <vector>
+#include <iostream>
+
+int main() {
+	int n;
+	std::vector<int> sequnce;
+	std::cin >> n;
+	
+	for (int i = 0; i < n; ++i) {
+		int count;
+		std::cin >> count;
+		sequnce.resize(count);
+		for (int i = 0; i < count; ++i)
+			std::scanf("%d", &sequnce[i]);
+		
