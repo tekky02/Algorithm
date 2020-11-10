a4e5a1e9ccd9a140ff60305e76910be2b5b8a5d1
Teddyky <1657961253@qq.com>
2020-11-10 13:27:24 +0800

    2020 11 week2 day2

---
+++ leetcode/challenge/2020_11_wk2_d2_maximum_difference_between_node_and_ancestor.go
@@ -0,0 +1,48 @@
+/*
+* @Author: Teddyky
+* @Date:   2020-11-10 13:26:26
+* @Last Modified by:   Teddyky
+* @Last Modified time: 2020-11-10 13:26:38
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxAncestorDiff(root *TreeNode) (ans int) {
+    helper(root, root.Val, &ans)
+    // temp := max(abs(root.Val - minv), abs(root.Val - maxv))
+    // ans = max(ans, temp)
+    return
+}
+
+func min(a, b int) int {
+    if a < b { return a }
+    return b
+}
+
+func max(a, b int) int {
+    if a > b { return a }
+    return b
+}
+
+func abs(num int) int {
+    if num < 0 { return -num }
+    return num
+}
+
+func helper(root *TreeNode, val int, ans *int) (int, int) {
+    if root != nil {
+        leftMin, leftMax := helper(root.Left, root.Val, ans)
+        rightMin, rightMax := helper(root.Right, root.Val, ans)
+        minv, maxv := min(leftMin, rightMin), max(leftMax, rightMax)
+        minv, maxv = min(minv, val), max(maxv, val)
+        temp := max(abs(val - minv), abs(val - maxv))
+        *ans = max(*ans, temp)
+        return minv, maxv
+    }
+    return val, val
+} 
