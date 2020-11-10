36783699dddb39924e97e801c65c61b9829bb6fe
Teddyky <1657961253@qq.com>
2020-11-10 13:43:42 +0800

    problem 1026 solution in C++

---
+++ leetcode/C++/1026_maximum_difference_between_node_and_ancestor.cc
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    int helper(TreeNode * root, int curmax, int curmin) {
+        if (root == nullptr) return curmax - curmin;
+        curmax = max(root->val, curmax);
+        curmin = min(root->val, curmin);
+        int l = helper(root->left, curmax, curmin);
+        int r = helper(root->right, curmax, curmin);
+        return max(l, r);
+    }
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return helper(root, root->val, root->val);
+    }
+};
