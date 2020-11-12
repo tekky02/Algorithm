c51e9c9dcba0a42343d382f87ae94132fed592a9
Teddyky <1657961253@qq.com>
2020-11-12 16:47:13 +0800

    leetcode problem 414 solution

---
+++ leetcode/Golang/414_third_maximum_number.go
@@ -0,0 +1,27 @@
+/*
+* @Author: Teddyky
+* @Date:   2020-11-12 16:46:34
+* @Last Modified by:   Teddyky
+* @Last Modified time: 2020-11-12 16:46:42
+*/
+func thirdMax(nums []int) int {
+    first, second, third := math.MinInt64, math.MinInt64, math.MinInt64
+    
+    for _, v := range nums {
+        if v == first || v == second || v == third {
+            continue
+        }
+        switch {
+        case v > first:
+            first, second, third = v, first, second
+        case v > second:
+            second, third = v, second
+        case v > third:
+            third = v
+        }
+    }
+    if third == math.MinInt64 {
+        return first
+    }
+    return third
+}
